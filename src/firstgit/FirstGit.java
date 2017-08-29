ed99451e65f9eb9a2a9cd8b67ca59ad2fe3f2b99
L117student <L117student@L117-08-042771.bitnet.wctc.edu>
2017-08-28 21:31:25 -0500

    Revert "Added Personal Flair"
    This reverts commit d9c07c3d5b55d44e7cff07fa3f154f73fc4c2e5a.

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -6,7 +6,7 @@ public class FirstGit {
 
     //@param args the command line arguments
     public static void main(String[] args) {
-        System.out.println("Goodbye World");
+        // TODO code application logic here
     }
     
 }
